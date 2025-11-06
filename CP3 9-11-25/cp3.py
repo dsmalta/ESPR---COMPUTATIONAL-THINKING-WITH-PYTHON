17cd404484ec50a235ddaaea795db3dfc111300c
dsmalta <davidwaffle30@gmail.com>
2025-11-06 19:38:17 -0300

    cp3 alterada

--- CP3 9-11-25/cp3.py
+++ CP3 9-11-25/cp3.py
@@ -1,6 +1,7 @@
 import os
 os.system ("cls")
 
+# Lista de vagas (começa com todas liberadas)
 vagas = [0,0,0,0,0,0,0,0,0,0]
 
 while True:
@@ -11,12 +12,17 @@ while True:
           4 - Encerrar o programa
           """)
     
+    # Escolha do usuário sobre o que deseja fazer
     opcao = int(input(f"Escolha: "))
     
+    # Enquanto o usuário não digitar uma opção válida, vai exibir uma mensagem de erro
     while opcao < 1 or opcao > 4:
         opcao = int(input(f"Opção inválida, digite um valor entre 1 e 4. "))
         
+    
     match opcao:
+        
+        # Primeira opção: mostrar o estado das vagas
         case 1:
             for ind in range (10):
                 num_vaga = ind + 1
@@ -24,23 +30,40 @@ while True:
                     print(f"Vaga {num_vaga}: Livre ") 
                 else:
                     print(f"Vaga {num_vaga}: Ocupada")
+                    
+        # Segunda opção: ocupar uma vaga
         case 2:
-            opcao_ocupa_vaga = int(input("Qual vaga você deseja ocupar? "))
-            ind = opcao_ocupa_vaga - 1   
+            opcao_ocupa_vaga = int(input(f"Qual vaga você deseja ocupar? "))
+            
+            # Mensagem de erro caso o número inserido seja inválido 
+            while opcao_ocupa_vaga < 1 or opcao_ocupa_vaga > 10:
+                opcao_ocupa_vaga = int(input((f"Insira uma vaga válida entre 1 e 10. Vaga: ")))
+                
+            ind = opcao_ocupa_vaga - 1 
+            
             if vagas[ind] == 0:
                 vagas[ind] = 1
                 print(f"Vaga ocupada com sucesso!")
-                        
+               
             else:
                 print(f"Essa vaga já está ocupada.")
+                
+        # Terceira opção: liberar uma vaga
         case 3:
-            opcao_libera_vaga = int(input("Qual vaga você deseja liberar? "))
+            opcao_libera_vaga = int(input(f"Qual vaga você deseja liberar? "))
+            
+            # Mensagem de erro caso o número inserido seja inválido
+            while opcao_libera_vaga < 1 or opcao_libera_vaga > 10:
+                opcao_libera_vaga = int(input((f"Insira uma vaga válida entre 1 e 10. Vaga: ")))
+            
             ind = opcao_libera_vaga - 1
             if vagas[ind] == 1:
                 vagas[ind] = 0
                 print(f"Vaga liberada com sucesso!")
             else: 
                 print(f"Essa vaga já está liberada.")
+                
+        # Última opção de encerrar o programa
         case 4:
             break
     

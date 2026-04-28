2f39c28cd55e49f7cc36ddcb0e60d2f77548432d
dsmalta <davidwaffle30@gmail.com>
2026-04-28 01:16:22 -0300

    fix: ajuste na formatacao das mensagens e ajuste nos procedimentos de cadastro e validacao de funcionario

--- 27-04-26_/exercicio2.py
+++ 27-04-26_/exercicio2.py
@@ -1,6 +1,6 @@
 func = {
     'cpf' : 0,
-    'nome' : 'Davi de Souza Malta',
+    'nome' : '',
     'salario' : 0,
 }
     
@@ -9,42 +9,28 @@ tabela = []
 def adicionar_funcionario(_tabela: list, _func: dict) -> None:
     _tabela.append(_func.copy())
 
-def cadastrar_funcionario(_func: dict, _tabela: list, _cpf: int, _nome: str, _salario: float) -> None:
+def funcionario_existente(_tabela: list, _cpf: int) -> bool:
+    for funcionario in _tabela:
+        if funcionario['cpf'] == _cpf:
+            return True
+    return False
 
-    print("\nCADASTRANDO FUNCIONÁRIO:")
-    print("=" * 30)
-    print(f"CPF.....: {_cpf}\n")
+def cadastrar_funcionario(_func: dict, _tabela: list, _cpf: int, _nome: str, _salario: float) -> None: 
+    _func['cpf'] = _cpf
+    _func['nome'] = _nome
+    _func['salario'] = _salario
 
-    existe = False
-
-    for f in _tabela:
-        if f['cpf'] == _cpf:
-            existe = True
-
-    if existe:
-        print("----------------------")
-        print("Funcionário já existe!")
-        print("----------------------")
-    else:
-        _func['cpf'] = _cpf
-        _func['nome'] = _nome
-        _func['salario'] = _salario
-
-        adicionar_funcionario(_tabela, _func)
-
-        print("----------------------")
-        print("Funcionário cadastrado!")
-        print("----------------------")
-
-    input("\nPressione ENTER para continuar...")
-
+    adicionar_funcionario(_tabela, _func)
+    resposta = 'Cadastrado com sucesso!'
+    print(f"\n{'!' * (len(resposta) + 1)}")
+    print(resposta)
+    print('!' * (len(resposta) + 1))
+    input('\nPressione uma tecla para continuar...')    
+    
+     
 def listar_funcionario(_tabela: list, _func: dict) -> None:
     for _func in _tabela:
         print(_func)
-
-# def listar_funcionario(_dicionario:)
-
-
 
 while True:
     print(f"""
@@ -63,17 +49,24 @@ while True:
             break
 
         case '1':
-            print("CADASTRANDO FUNCIONÁRIO: ")
-            print("=" * 30)
+            mensagem = "\nCADASTRANDO FUNCIONÁRIO:"
+            print(mensagem)
+            print("=" * len(mensagem))
             print()
             cpf = int(input("CPF.....: "))
-            nome = input("Nome....: ")
-            salario = int(input("Salário.: "))
-
-            cadastrar_funcionario(func, tabela, cpf, nome, salario)
-            adicionar_funcionario(tabela, func)
+            
+            if funcionario_existente(tabela, cpf):
+                resposta = "Funcionario ja existente"
+                print(f"\n{'-' * (len(resposta) + 1)}")
+                print(resposta)
+                print('-' * (len(resposta) + 1))
+                input('\nPressione uma tecla para continuar...')
+            else:
+                nome = input("Nome....: ")
+                salario = int(input("Salário.: "))
+                cadastrar_funcionario(func, tabela, cpf, nome, salario)
 
         case '5':
             print("CPF", '' * 10, "NOME", '' * 10, "SALARIO")
             print("=" * 30)
-            print(listar_funcionario(tabela, func))
+            listar_funcionario(tabela, func)
